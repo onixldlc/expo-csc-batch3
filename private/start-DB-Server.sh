c777451ed1a7525e8cee7fd0ea4cfc0fffd26829
onixldlc <onxdl16427@outlook.com>
2021-08-09 21:11:04 +0700

    another-minor-improvement

--- private/start-DB-Server.sh
+++ private/start-DB-Server.sh
@@ -1,1 +1,4 @@
-mongod --dbpath=/home/mamadz/Documents/codes/github/csc-expo/expo-csc-batch3/private/db
+currentdir=$(pwd)
+dbpath=$(echo ${currentdir}/db)
+mongod --dbpath=${dbpath}
+#mongod --dbpath=/home/mamadz/Documents/codes/github/csc-expo/expo-csc-batch3/private/db
